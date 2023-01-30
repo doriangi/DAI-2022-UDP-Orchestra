2798d1851f10edab19595161b6ef8e58b646ef92
Dorian Gillioz <gilliozdorian@gmail.com>
2023-01-30 18:26:26 +0100

    finis normalement

--- docker/image-auditor/src/app.js
+++ docker/image-auditor/src/app.js
@@ -1,50 +1,54 @@
 const dgram = require('dgram');
 const net = require('net');
-const protocol = require('./auditor_protocol');
 
 // Intervalle max où le musicien est inactif
 const INTERVAL = 5000;
+const MULTICAST_ADDRESS = "231.2.23.2";
+const MULTICAST_PORT = 3000;
 const TCP_PORT = 2205;
 
-const socket = dgram.createSocket('udp4');
-const server = net.createServer();
+var sounds = new Map();
+sounds.set("ti-ta-ti", "piano");
+sounds.set("pouet", "trumpet");
+sounds.set("trulu", "flute");
+sounds.set("gzi-gzi", "violin");
+sounds.set("boum-boum", "drum");
 
 const musicians = new Map();
-const instruments = protocol.INSTRUMENTS;
 
-function onMessage(msg, source) {
-    let data = {
-        ...JSON.parse(msg),
-        last: Date.now()
-    };
-    data.instrument = Object.keys(instruments).find((i) => instruments[i] === data.sound);
+const socket = dgram.createSocket('udp4');
+
+socket.bind(MULTICAST_PORT, () => {
+    socket.addMembership(MULTICAST_ADDRESS);
+});
+
+socket.on('message', (msg, source) => {
+    let data = JSON.parse(msg);
+    data.last = Date.now();
+    data.instrument = sounds.get(data.sound);
     data.activeSince = musicians.has(data.uuid) ? musicians.get(data.uuid).activeSince : data.last;
+
     delete data.sound;
 
     musicians.set(data.uuid, data);
 
-    console.log('Des données sont arrivées: ' + msg + '. Sur le port : ' + source.port);
-}
+    console.log('Message reçu : ' + msg);
+});
 
-function onConnection(socket) {
-    const content = Array.from(musicians.entries()).filter(([uuid, musician]) => {
-        const remove = Date.now() - musician.last > INTERVAL;
-        if (remove) musicians.delete(uuid);
-        return !remove;
+const server = net.createServer();
+
+server.on('connection', (socket) => {
+    const actives = Array.from(musicians.entries()).filter(([uuid, musician]) => {
+        const active = Date.now() - musician.last <= INTERVAL;
+        if (!active) musicians.delete(uuid);
+        return active;
     }).map(([uuid, musician]) => ({
-        uuid,
+        uuid: uuid,
         instrument: musician.instrument,
         activeSince: new Date(musician.activeSince)
     }));
-    socket.write(JSON.stringify(content) + '\n');
+    socket.write(JSON.stringify(actives) + '\n');
     socket.end();
-}
-
-socket.bind(protocol.MULTICAST_PORT, () => {
-    console.log('Rejoins groupe multicast');
-    socket.addMembership(protocol.MULTICAST_ADDRESS);
 });
 
-socket.on('message', onMessage);
 server.listen(TCP_PORT);
-server.on('connection', onConnection);
--- docker/image-musician/src/app.js
+++ docker/image-musician/src/app.js
@@ -1,30 +1,33 @@
 const dgram = require('dgram');
 const crypto = require('crypto');
-const protocol = require('./musician_protocol');
+
+const MULTICAST_ADDRESS = "231.2.23.2";
+const MULTICAST_PORT = 3000;
+
+let instruments = new Map();
+instruments.set("piano", "ti-ta-ti");
+instruments.set("trumpet", "pouet");
+instruments.set("flute", "trulu");
+instruments.set("violin", "gzi-gzi");
+instruments.set("drum", "boum-boum");
 
 const socket = dgram.createSocket('udp4');
 const uuid = crypto.randomUUID();
 const INTERVAL = 1000;
 
 const instrumentName = process.argv[2];
-let msg;
 
-if (!instrumentName || !protocol.INSTRUMENTS.hasOwnProperty(instrumentName)) {
-    console.log('Instrument manquant ou incorrect');
-    return;
+const data = {};
+data.uuid = uuid;
+data.sound = instruments.get(instrumentName);
+
+const message = JSON.stringify(data);
+
+let payload = new Buffer.from(message);
+
+function sendData() {
+    socket.send(payload, 0, payload.length, MULTICAST_PORT, MULTICAST_ADDRESS);
+    console.log('Envoi du message : ' + message);
 }
 
-const payload = JSON.stringify({
-    uuid,
-    sound: protocol.INSTRUMENTS[instrumentName]
-});
-
-msg = Buffer.from(payload);
-
-function update() {
-    socket.send(msg, 0, msg.length, protocol.MULTICAST_PORT, protocol.MULTICAST_ADDRESS, () => {
-        console.log('Envoi du payload: ' + payload + ' avec le port ' + socket.address().port);
-    });
-}
-
-setInterval(update, INTERVAL);
+setInterval(sendData, INTERVAL);
